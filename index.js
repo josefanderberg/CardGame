f92f16e7f14d0d94a766e484ecb9f8ba88483e14
josefanderberg <josef.anderberg@gmail.com>
2025-10-15 16:07:40 +0200

    Implement card shuffling and dealing logic for multiple players

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const färger = ['Hjärter', 'Ruter', 'Spader', 'Klöver']
 const värde = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 const namn = ['Ess', 'Två', 'Tre', 'Fyra', 'Fem', 'Sex','Sju', 'Åtta', 'Nio', 'Tio', 'Knekt', 'Dam', 'Kung']
+const spelare = [
+    { person: 1, hand:[]},
+    { person: 2, hand:[]},
+    { person: 3, hand:[]},
+    { person: 4, hand:[]}
+]
 
-const kort = []
-
+let kort = [
+    {
+    färger: null,
+    namn: 'joker',
+    värde: 0
+    }
+]
 
 for (f = 0; f < färger.length; f++)
     {
@@ -20,8 +31,84 @@ for (f = 0; f < färger.length; f++)
             }
     }
 
-    for (i = 0; i < kort.length; i++)
+
+
+    kort = Shuffle(kort)
+    DelaUt(kort, spelare)
+
+    console.dir(spelare, { depth: null }) 
+    console.log(kort)
+
+
+
+
+    function Shuffle(kort)
+    {
+        for (i = 0; i < kort.length; i++)
+        {
+            kort[i].sorteringsnummer = Math.random();
+        }
+
+        kort.sort((a, b) => a.sorteringsnummer - b.sorteringsnummer);
+
+        for (i = 0; i < kort.length; i++)
+        {
+            delete kort[i].sorteringsnummer
+        }
+
+        return kort;
+    }
+
+    function DelaUt(kort, spelare) 
+    {
+        const restKvar = kort.length % spelare.length; 
+    
+        let spelarIndex = 0
+    
+        while(kort.length > restKvar)
+        {
+            spelare[spelarIndex].hand.push(kort.pop())
+    
+            if (spelarIndex == spelare.length - 1)
+            {
+                spelarIndex = 0
+            }
+            else {
+                spelarIndex++
+            }
+        }
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+    function Blanda(kort)
+    {
+        let blandadeKort = []
+
+        while (kort.length > 0)
+        {
+
+            let slumpIndex = Math.floor(Math.random() * kort.length)
+            let randomKort = kort.splice(slumpIndex, 1)
+            blandadeKort.push(randomKort[0])
+            
+        }
+        return blandadeKort
+    }
+/*     for (i = 0; i < kort.length; i++)
     {
         console.log(i + 1, kort[i])
-    }
+
+        
+    } */
  
+    
